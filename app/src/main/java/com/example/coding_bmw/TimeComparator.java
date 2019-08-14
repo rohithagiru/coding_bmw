980d4011a287fa0fa54f35031709383a90f482fd
rohithagiru <rohithkumaragiru@gmail.com>
2019-08-14 12:34:49 -0500

    Bmw coding challenge-consuming the JSON using retrofit with restful aps

---
+++ app/src/main/java/com/example/coding_bmw/TimeComparator.java
@@ -0,0 +1,10 @@
+package com.example.coding_bmw;
+
+import java.util.Comparator;
+
+public class TimeComparator implements Comparator {
+    @Override
+    public int compare(Object o, Object t1) {
+        return 0;
+    }
+}
